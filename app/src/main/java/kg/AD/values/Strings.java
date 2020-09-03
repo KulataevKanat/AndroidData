e3ea14939ec5c058f7cd98d7190bca6bd0ddb2c4
KulataevKanat <RaketaEnot4466@gmail.com>
2020-09-03 17:37:18 +0600

    added new lines

--- app/src/main/java/kg/AD/values/Strings.java
+++ app/src/main/java/kg/AD/values/Strings.java
@@ -4,4 +4,5 @@ public class Strings {
     public final static String EXTRA_MESSAGE = "EXTRA_MESSAGE";
     public final static String HELLO_JAVA_ELEMENT = "Hello Java Interface!";
     public final static String HELLO_XML_ELEMENT = "Hello XML Interface!";
+    public final static String JAVA_UNIT_ELEMENT = "В метод setLayoutParams() передается объект ViewGroup.LayoutParams. Этот объект инициализируется двумя параметрами: шириной и высотой.";
 }
