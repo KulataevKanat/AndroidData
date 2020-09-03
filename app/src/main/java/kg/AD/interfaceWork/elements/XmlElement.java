8f9a524d21f201e62da39162ae293a2f0183a28e
KulataevKanat <RaketaEnot4466@gmail.com>
2020-09-03 14:54:12 +0600

    XmlElement

---
+++ app/src/main/java/kg/AD/interfaceWork/elements/XmlElement.java
@@ -0,0 +1,39 @@
+package kg.AD.interfaceWork.elements;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import kg.AD.R;
+import kg.AD.values.Classes;
+import kg.AD.values.Strings;
+
+public class XmlElement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.interface_activity_xml_element);
+        TextView textView = (TextView) findViewById(R.id.tv_xml);
+        textView.setText(Strings.HELLO_XML_ELEMENT);
+        textView.setTextSize(30);
+    }
+
+    /*public void chapterButton() {
+        Button layout_change = (Button) findViewById(R.id.next_element);
+        layout_change.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), Classes.XML_ELEMENT);
+                        startActivity(intent);
+                    }
+                }
+        );
+    }*/
+
+}
