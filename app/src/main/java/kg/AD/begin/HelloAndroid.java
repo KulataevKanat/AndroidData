7cc6592db4555eaac4a200a6db563a1cea74010f
KulataevKanat <RaketaEnot4466@gmail.com>
2020-09-03 14:51:39 +0600

    HelloAndroid

---
+++ app/src/main/java/kg/AD/begin/HelloAndroid.java
@@ -0,0 +1,34 @@
+package kg.AD.begin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import kg.AD.R;
+import kg.AD.values.Classes;
+
+public class HelloAndroid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.begin_activity_android_hello);
+        chapterButton();
+    }
+
+    private void chapterButton() {
+        Button layout_change = (Button) findViewById(R.id.next_graphical);
+        layout_change.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), Classes.GRAPHICAL_APPLICATION);
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+}
