95ad78745952a96efbc2ef6f911d5a0191c14b90
KulataevKanat <RaketaEnot4466@gmail.com>
2020-09-03 17:40:22 +0600

    JavaUnits

---
+++ app/src/main/java/kg/AD/interfaceWork/containerElements/JavaUnits.java
@@ -0,0 +1,28 @@
+package kg.AD.interfaceWork.containerElements;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import kg.AD.values.Strings;
+
+public class JavaUnits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        TextView textView = new TextView(this);
+        textView.setText(Strings.JAVA_UNIT_ELEMENT);
+        textView.setTextSize(30);
+
+        textView.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        relativeLayout.addView(textView);
+        setContentView(relativeLayout);
+    }
+}
