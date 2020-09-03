c77b467e7b10748afb5bb5859d1b6d131d9aee8c
KulataevKanat <RaketaEnot4466@gmail.com>
2020-09-03 14:50:19 +0600

    Strings

---
+++ app/src/main/java/kg/AD/values/Strings.java
@@ -0,0 +1,7 @@
+package kg.AD.values;
+
+public class Strings {
+    public final static String EXTRA_MESSAGE = "EXTRA_MESSAGE";
+    public final static String HELLO_JAVA_ELEMENT = "Hello Java Interface!";
+    public final static String HELLO_XML_ELEMENT = "Hello XML Interface!";
+}
