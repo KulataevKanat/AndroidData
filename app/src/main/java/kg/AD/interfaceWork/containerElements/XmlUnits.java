2ccfd1247e864e7fa1175f7f8c66d1f302491314
KulataevKanat <RaketaEnot4466@gmail.com>
2020-09-03 17:39:56 +0600

    XmlUnits

---
+++ app/src/main/java/kg/AD/interfaceWork/containerElements/XmlUnits.java
@@ -0,0 +1,45 @@
+package kg.AD.interfaceWork.containerElements;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import kg.AD.R;
+import kg.AD.values.Classes;
+
+public class XmlUnits extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.interface_activity_xml_units);
+        chapterButton();
+    }
+
+    public void chapterButton() {
+        Button layout_change = (Button) findViewById(R.id.next_indents);
+        Button layout_java_change = (Button) findViewById(R.id.next_units_java_element);
+        layout_change.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), Classes.XML_UNITS);
+                        startActivity(intent);
+                    }
+                }
+        );
+        layout_java_change.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), Classes.JAVA_UNITS);
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+
+}
