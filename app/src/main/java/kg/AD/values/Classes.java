a581e042e155549efca7fb617c938569cf05b80a
KulataevKanat <RaketaEnot4466@gmail.com>
2020-09-03 17:14:39 +0600

    added comments

--- app/src/main/java/kg/AD/values/Classes.java
+++ app/src/main/java/kg/AD/values/Classes.java
@@ -2,19 +2,30 @@ package kg.AD.values;
 
 import kg.AD.begin.*;
 import kg.AD.chapters.*;
+import kg.AD.interfaceWork.containerElements.Units;
 import kg.AD.interfaceWork.elements.JavaElement;
 import kg.AD.interfaceWork.elements.XmlElement;
 
 public class Classes {
-    //begin packages
+    /**
+     * begin packages:
+     */
     public final static Class<?> HELLO_ANDROID = HelloAndroid.class;
     public final static Class<?> GRAPHICAL_APPLICATION = GraphicApplication.class;
 
-    //interfaceWork packages
+    /**
+     * interfaceWork packages:
+     */
+    //containerElements
     public final static Class<?> JAVA_ELEMENT = JavaElement.class;
     public final static Class<?> XML_ELEMENT = XmlElement.class;
+    //elements
+    public final static Class<?> UNITS = Units.class;
 
-    //chapters packages
+
+    /**
+     * chapters packages:
+     */
     public final static Class<?> BEGIN_CHAPTER = BeginChapter.class;
     public final static Class<?> INTERFACE_CHAPTER = InterfaceChapter.class;
 
