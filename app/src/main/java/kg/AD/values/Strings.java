0f492a4c7d99c3c9a4f15221e23b713896117984
KulataevKanat <RaketaEnot4466@gmail.com>
2020-09-04 21:05:52 +0600

    changed line

--- app/src/main/java/kg/AD/values/Strings.java
+++ app/src/main/java/kg/AD/values/Strings.java
@@ -4,8 +4,8 @@ public class Strings {
     public final static String EXTRA_MESSAGE = "EXTRA_MESSAGE";
     public final static String HELLO_JAVA_ELEMENT = "Hello Java Interface!";
     public final static String HELLO_XML_ELEMENT = "Hello XML Interface!";
-    public final static String JAVA_UNIT_ELEMENT = "В метод setLayoutParams() передается объект ViewGroup.LayoutParams. Этот объект инициализируется двумя параметрами: шириной и высотой.";
+    public final static String JAVA_UNIT_ELEMENT = "px = dp * (dpi / 160)";
     public final static String HELLO = "Hello";
-    public final static String JAVA= "JAVA";
-    public final static String CODE= "CODE";
+    public final static String JAVA = "JAVA";
+    public final static String CODE = "CODE";
 }
