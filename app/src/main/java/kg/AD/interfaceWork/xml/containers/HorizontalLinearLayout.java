ed6e674cc58761f90c2f7c8f3b32cbbc1ace3690
KulataevKanat <RaketaEnot4466@gmail.com>
2020-09-04 18:06:47 +0600

    HorizontalLinearLayout

---
+++ app/src/main/java/kg/AD/interfaceWork/xml/containers/HorizontalLinearLayout.java
@@ -0,0 +1,18 @@
+package kg.AD.interfaceWork.xml.containers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kg.AD.R;
+
+public class HorizontalLinearLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horizontal_linear_layout);
+    }
+
+
+}
