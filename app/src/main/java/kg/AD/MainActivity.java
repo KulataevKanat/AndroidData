db9eee7b4cf34859c289b7d68465d672447ec0fd
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-09-11 02:27:16 +0600

    added new buttons

--- app/src/main/java/kg/AD/MainActivity.java
+++ app/src/main/java/kg/AD/MainActivity.java
@@ -21,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
     private void chapterButton() {
         Button begin = (Button) findViewById(R.id.chapter1);
         Button interface_work = (Button) findViewById(R.id.chapter2);
+        Button main_elements = (Button) findViewById(R.id.chapter3);
+        Button resources = (Button) findViewById(R.id.chapter4);
+        Button activity = (Button) findViewById(R.id.chapter5);
+        Button image = (Button) findViewById(R.id.chapter6);
+        Button adapters_and_lists = (Button) findViewById(R.id.chapter7);
         begin.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -35,6 +40,51 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         Intent intent = new Intent(getApplicationContext(), Classes.INTERFACE_CHAPTER);
+                        startActivity(intent);
+                    }
+                }
+        );
+        main_elements.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), Classes.MAIN_ELEMENTS_CHAPTER);
+                        startActivity(intent);
+                    }
+                }
+        );
+        resources.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), Classes.RESOURCES_CHAPTER);
+                        startActivity(intent);
+                    }
+                }
+        );
+        activity.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), Classes.ACTIVITY_CHAPTER);
+                        startActivity(intent);
+                    }
+                }
+        );
+        image.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), Classes.IMAGE_CHAPTER);
+                        startActivity(intent);
+                    }
+                }
+        );
+        adapters_and_lists.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(getApplicationContext(), Classes.ADAPTERS_AND_LISTS_CHAPTER);
                         startActivity(intent);
                     }
                 }
