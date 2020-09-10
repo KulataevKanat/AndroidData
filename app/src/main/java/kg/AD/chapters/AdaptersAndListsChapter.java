3b1fa1041b55e930c78ef29de14a9ba59c4e8fe1
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-09-11 02:30:18 +0600

    AdaptersAndListsChapter

---
+++ app/src/main/java/kg/AD/chapters/AdaptersAndListsChapter.java
@@ -0,0 +1,16 @@
+package kg.AD.chapters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kg.AD.R;
+
+public class AdaptersAndListsChapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adapters_and_lists);
+    }
+}
