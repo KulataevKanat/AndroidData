d00c178a80857c336bf86e119aeb280ba0112bf6
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-09-11 02:30:43 +0600

    ImageChapter

---
+++ app/src/main/java/kg/AD/chapters/ImageChapter.java
@@ -0,0 +1,16 @@
+package kg.AD.chapters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kg.AD.R;
+
+public class ImageChapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+    }
+}
