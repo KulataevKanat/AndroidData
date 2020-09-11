5ce09981754a11b5682d81b05a8914fa3c5006f0
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-09-12 03:25:35 +0600

    ButtonPanel

---
+++ app/src/main/java/kg/AD/interfaceWorks/xml/elements/ButtonPanel.java
@@ -0,0 +1,16 @@
+package kg.AD.interfaceWorks.xml.elements;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kg.AD.R;
+
+public class ButtonPanel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_panel);
+    }
+}
