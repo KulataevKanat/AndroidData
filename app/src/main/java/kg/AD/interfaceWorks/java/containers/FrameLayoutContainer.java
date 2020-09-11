19b5b0d77ec61484e1acf0909f77230e121deb14
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-09-11 15:16:29 +0600

    FrameLayoutContainer

---
+++ app/src/main/java/kg/AD/interfaceWorks/java/containers/FrameLayoutContainer.java
@@ -0,0 +1,36 @@
+package kg.AD.interfaceWorks.java.containers;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import kg.AD.values.Strings;
+
+public class FrameLayoutContainer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FrameLayout frameLayout = new FrameLayout(this);
+
+        TextView textView = new TextView(this);
+        textView.setText(Strings.HELLO_JAVA_ELEMENT);
+        textView.setTextSize(30);
+
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT);
+
+        layoutParams.gravity = Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM;
+
+        textView.setLayoutParams(layoutParams);
+        frameLayout.addView(textView);
+        setContentView(frameLayout);
+    }
+
+}
