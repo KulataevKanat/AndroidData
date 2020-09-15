4a31d4c2895007f8bf459dd8e901165734b932ba
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-09-16 00:35:45 +0600

    added functional for Button

--- app/src/main/java/kg/AD/chapters/MainElementsChapter.java
+++ app/src/main/java/kg/AD/chapters/MainElementsChapter.java
@@ -2,9 +2,13 @@ package kg.AD.chapters;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import kg.AD.R;
+import kg.AD.values.Classes;
 
 public class MainElementsChapter extends AppCompatActivity {
 
@@ -12,5 +16,17 @@ public class MainElementsChapter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_elements);
+        chapterButton();
+    }
+
+    private void chapterButton() {
+        Button next_layout = (Button) findViewById(R.id.next_tv);
+        next_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Classes.XML_TEXT_VIEW);
+                startActivity(intent);
+            }
+        });
     }
 }
