fa4401ace0f9ea9f4913ae69374189820460daa8
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-09-16 00:39:34 +0600

    added new Colors

--- app/src/main/java/kg/AD/values/Colors.java
+++ app/src/main/java/kg/AD/values/Colors.java
@@ -6,4 +6,8 @@ public class Colors {
     public final static String WHITE = "#FFFFFF";
     public final static String GRAY = "#808080";
     public final static String DARK_VIOLET = "#9400D3";
+    public final static String BROWN = "#A52A2A";
+    public final static String FOREST_GREEN = "#228B22";
+
+
 }
