0d9085c6c6ba2c73b5b838f56f5cb631f86dd666
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-09-22 04:27:05 +0600

    Pickers

--- app/src/main/java/kg/AD/mainElements/xml/Pickers.java
+++ app/src/main/java/kg/AD/mainElements/xml/Pickers.java
@@ -22,36 +22,13 @@ public class Pickers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pickers);
-        pickers();
         chapterButton();
-    }
-
-    @SuppressLint("SetTextI18n")
-    public void pickers() {
-        TextView dateDefault = (TextView) findViewById(R.id.dateDefault);
-        TextView timeDefault = (TextView) findViewById(R.id.timeDefault);
-        DatePicker dp = (DatePicker) findViewById(R.id.datePicker);
-        TimePicker tp = (TimePicker) findViewById(R.id.timePicker);
-        Formatter timeF = new Formatter();
-
-        dateDefault.setText("Дата по умолчанию " +
-                dp.getDayOfMonth() +
-                "/" +
-                (dp.getMonth() + 1) +
-                "/" +
-                dp.getYear()
-        );
-        dp.init(2020, 6, 4, null);
-
-        timeF.format("Время по умолчанию %d:%02d", tp.getCurrentHour(), tp.getCurrentMinute());
-        timeDefault.setText(timeF.toString());
-        tp.setIs24HourView(true);
-        tp.setCurrentHour(10);
-        tp.setCurrentMinute(10);
     }
 
     private void chapterButton() {
         Button change_layout = (Button) findViewById(R.id.next_seekBar);
+        Button dp = (Button) findViewById(R.id.next_dp);
+        Button tp = (Button) findViewById(R.id.next_tp);
 
         change_layout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,5 +37,22 @@ public class Pickers extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        dp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Classes.DATE_PICKER);
+                startActivity(intent);
+            }
+        });
+
+        tp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Classes.TIME_PICKER);
+                startActivity(intent);
+            }
+        });
     }
 }
+
