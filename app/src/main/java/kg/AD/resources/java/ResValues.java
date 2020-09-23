bd4f9889b8cee7443fc89578125882d34a2dbfad
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-09-23 06:52:27 +0600

    added container added plural functional

--- app/src/main/java/kg/AD/resources/java/ResValues.java
+++ app/src/main/java/kg/AD/resources/java/ResValues.java
@@ -1,16 +1,20 @@
 package kg.AD.resources.java;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import kg.AD.R;
 
 public class ResValues extends AppCompatActivity {
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,14 +23,48 @@ public class ResValues extends AppCompatActivity {
         String hello_android = getResources().getString(R.string.hello_android);
         // получение ресурсов из файла colors.xml
         int hello_android_color = getResources().getColor(R.color.dark_green);
+        int chapter_color = getResources().getColor(R.color.dark_magenta);
         // получение ресурсов из файла dimens.xml
-        float hello_android_dimenSP = getResources().getDimension(R.dimen.twentiesSP);
+        float dimenSP = getResources().getDimension(R.dimen.twentiesSP);
+        // получение ресурсов из файла plurals.xml
+        String chapters = getResources().getQuantityString(R.plurals.chapters, 25, 25);
+
+        ConstraintLayout constraintLayout = new ConstraintLayout(this);
 
         TextView textView = new TextView(this);
+        textView.setId(1);
         textView.setText(hello_android);
-        textView.setTextSize(hello_android_dimenSP);
-        textView.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL);
+        textView.setTextSize(dimenSP);
         textView.setBackgroundColor(hello_android_color);
-        setContentView(textView);
+        textView.setGravity(Gravity.CENTER);
+
+        ConstraintLayout.LayoutParams tv = new ConstraintLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+
+        tv.topToTop = constraintLayout.getTop();
+        tv.bottomToBottom = constraintLayout.getBottom();
+        tv.rightToRight = constraintLayout.getRight();
+        tv.leftToLeft = constraintLayout.getLeft();
+        constraintLayout.addView(textView, tv);
+
+        TextView textPlural = new TextView(this);
+        textPlural.setText(chapters);
+        textPlural.setTextSize(dimenSP);
+        textPlural.setBackgroundColor(chapter_color);
+        textPlural.setGravity(Gravity.CENTER);
+
+        ConstraintLayout.LayoutParams tp = new ConstraintLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+
+        tp.topToBottom = textView.getId();
+        tp.rightToRight = constraintLayout.getRight();
+        tp.leftToLeft = constraintLayout.getLeft();
+        constraintLayout.addView(textPlural, tp);
+
+        setContentView(constraintLayout);
     }
 }
