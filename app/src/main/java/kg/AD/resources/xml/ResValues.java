b41a28d10e99fa16471a4bec474a10b58d364151
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-09-26 03:23:45 +0600

    added button functions

--- app/src/main/java/kg/AD/resources/xml/ResValues.java
+++ app/src/main/java/kg/AD/resources/xml/ResValues.java
@@ -2,9 +2,13 @@ package kg.AD.resources.xml;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import kg.AD.R;
+import kg.AD.values.Classes;
 
 public class ResValues extends AppCompatActivity {
 
@@ -12,5 +16,10 @@ public class ResValues extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_res_values);
+        Button next_chapter = (Button) findViewById(R.id.next_chapter5);
+        next_chapter.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), Classes.ACTIVITY_CHAPTER);
+            startActivity(intent);
+        });
     }
 }
