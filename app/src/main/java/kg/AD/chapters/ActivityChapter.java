0440c8eeb2d070aeb275f1c5ce475d41bb3dc65c
kulataev_kanat <Kulataevkanat01@gmail.com>
2020-09-26 03:25:25 +0600

    added button functional

--- app/src/main/java/kg/AD/chapters/ActivityChapter.java
+++ app/src/main/java/kg/AD/chapters/ActivityChapter.java
@@ -2,9 +2,12 @@ package kg.AD.chapters;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import kg.AD.R;
+import kg.AD.values.Classes;
 
 public class ActivityChapter extends AppCompatActivity {
 
@@ -12,5 +15,10 @@ public class ActivityChapter extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity);
+        Button change_layout = (Button) findViewById(R.id.next_activity_methods);
+        change_layout.setOnClickListener(v->{
+            Intent intent = new Intent(getApplicationContext(), Classes.METHODS);
+            startActivity(intent);
+        });
     }
 }
